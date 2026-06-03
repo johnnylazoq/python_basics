c9dfa60c2d9c15b734fef6806184ded24652f2cc
jolaz <johnny.lazoq.com>
2026-06-03 09:43:28 +0200

    numbers

---
+++ numbers.py
@@ -0,0 +1,27 @@
+from math import *
+
+def main():
+    my_num = -5
+    print(my_num)
+    print(str(my_num) + " is my favorite number.")
+
+    my_num = 3.14
+    print(my_num)
+
+    my_num = -2.5
+    print(my_num)
+
+    my_num = 4
+    print(my_num)
+    print(abs(my_num))  
+    print(pow(3, 2))
+    print(max(4, 6))
+    print(min(4, 6))
+    print(round(3.7))
+    print(round(3.2))
+    print(floor(3.7))
+    print(ceil(3.2))
+    print(sqrt(36))
+
+if __name__ == "__main__":
+    main()
