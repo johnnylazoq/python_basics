830fa5a2a03eb5de244b25e3d24f8d7f92898bdb
jolaz <johnny.lazoq.com>
2026-06-03 09:26:20 +0200

    variables lecture

---
+++ variables.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    character_name = "John"
+    character_age = "56.66"
+    is_male = True
+
+    print("There was a man named " + character_name + ",")
+    print("he was " + character_age + " years old.")
+    if is_male:
+        print("He liked his name.")             
+
+    character_name = "Mike"
+    print("He really liked the name " + character_name + ",")   
+    print("but didn't like being " + character_age + ".") 
+
+if __name__ == "__main__":
+    main()
