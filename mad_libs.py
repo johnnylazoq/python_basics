2bca2238072cf52c990cfffe3c01334ba349008a
jolaz <johnny.lazoq.com>
2026-06-03 09:55:19 +0200

    mad_libs

---
+++ mad_libs.py
@@ -0,0 +1,11 @@
+def main():
+    color = input("Enter a color:")
+    plural_noun = input("Enter a plural noun:")
+    celebrity = input("Enter a celebrity:")
+
+    print("Roses are " + color)
+    print(plural_noun + " are blue")
+    print("I love " + celebrity)
+
+if __name__ == "__main__":
+    main()
